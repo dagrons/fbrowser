4ffaea2943e0c4e5271f06268f092e1d45be5e40
dagrons <heyuehuii@126.com>
2022-05-03 20:00:00 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="filebrowser",
-    version="1.0",
+    version="1.1",
     description="a wrapper for filebrowser http api",
     author="dagrons",
     author_email="heyuehuii@126.com",
