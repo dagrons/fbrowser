282ad44a029f94855e8ab304cc5f197f5d7ec9e1
dagrons <heyuehuii@126.com>
2022-05-25 04:06:00 +0800

    private some funcs

--- filebrowser/filebrowser.py
+++ filebrowser/filebrowser.py
@@ -57,18 +57,18 @@ class Machine(FileBrowserClientIFace):
         return self
 
     def download_shared_file(self, code: str, fpath: str, share_password: str, save_path: str):
-        url = self.build_share_download_url(code, fpath, self.get_share_token(code, share_password))
-        self.download(save_path, url)
+        url = self.__build_share_download_url(code, fpath, self.__get_share_token(code, share_password))
+        self.__download(save_path, url)
 
     def download_auth_file(self, fpath: str, save_path: str):
         if not self.is_authenticated():
             raise NoCredentialsException()
-        url = self.build_auth_download_url(fpath)
-        self.download(save_path, url, headers={
+        url = self.__build_auth_download_url(fpath)
+        self.__download(save_path, url, headers={
             "X-Auth": self.auth_token,
         })
 
-    def download(self, save_path: str, url: str, headers={}):
+    def __download(self, save_path: str, url: str, headers={}):
         resp = self.s.get(url, headers=headers)
         if resp.status_code != 200:
             raise DownLoadException(resp.status_code, url)
@@ -78,7 +78,7 @@ class Machine(FileBrowserClientIFace):
     def is_authenticated(self) -> bool:
         return self.auth_token is not None
 
-    def get_share_token(self, code: str, share_password: str) -> str:
+    def __get_share_token(self, code: str, share_password: str) -> str:
         if code in self.share_token_cache:
             return self.share_token_cache[code]
         file_url = build_url_from_base_url(self.base_url, f'/api/public/share/{code}')
@@ -91,10 +91,10 @@ class Machine(FileBrowserClientIFace):
         self.share_token_cache[code] = token
         return token
 
-    def build_share_download_url(self, code: str, fpath: str, token: str) -> str:
+    def __build_share_download_url(self, code: str, fpath: str, token: str) -> str:
         return build_url_from_base_url(self.base_url, f'/api/public/dl/{code}/{fpath}', query={
             "token": token,
         })
 
-    def build_auth_download_url(self, fpath: str) -> str:
+    def __build_auth_download_url(self, fpath: str) -> str:
         return build_url_from_base_url(self.base_url, f'/api/raw/{fpath}')
